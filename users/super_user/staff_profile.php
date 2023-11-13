721a2d409223dc6f2c593735e8a0811ba57369bc
Wezzie Mhango <wezmhango@hotmail.com>
2023-11-13 15:06:47 +0200

    updated staff profiles

--- users/super_user/staff_profile.php
+++ users/super_user/staff_profile.php
@@ -38,11 +38,9 @@
     <div class="d-flex w-100">
       <div class="min-width-340">
         <div class="border-end user-chat-box h-100">
-          <div class="px-4 pt-9 pb-6 d-none d-lg-block">
-            <form class="position-relative">
-              <input type="text" class="form-control search-chat py-2 ps-5" id="text-srh" placeholder="Search" />
-              <i class="ti ti-search position-absolute top-50 start-0 translate-middle-y fs-6 text-dark ms-3"></i>
-            </form>
+          <div class="p-9 py-3 border-bottom chat-meta-user d-flex align-items-center justify-content-between">
+            <h5 class="text-dark mb-0 fw-semibold">Muscco Staff</h5>
+            
           </div>
           <div class="app-chat">
             <ul class="chat-users" style="height: calc(150vh - 400px)" data-simplebar>
@@ -143,47 +141,6 @@
           </div>
         </div>
       </div>
-    </div>
-    <div class="offcanvas offcanvas-start user-chat-box" tabindex="-1" id="chat-sidebar" aria-labelledby="offcanvasExampleLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasExampleLabel"> Contact </h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="px-9 pt-4 pb-3">
-        <button class="btn btn-primary fw-semibold py-8 w-100">Add New Contact</button>
-      </div>
-      <ul class="list-group" style="height: calc(100vh - 150px)" data-simplebar>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-inbox fs-5"></i>All Contacts </a>
-        </li>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-star"></i>Starred </a>
-        </li>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-file-text fs-5"></i>Pening Approval </a>
-        </li>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-alert-circle"></i>Blocked </a>
-        </li>
-        <li class="border-bottom my-3"></li>
-        <li class="fw-semibold text-dark text-uppercase mx-9 my-2 px-3 fs-2">CATEGORIES</li>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-bookmark fs-5 text-primary"></i>Engineers </a>
-        </li>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-bookmark fs-5 text-warning"></i>Support Staff </a>
-        </li>
-        <li class="list-group-item border-0 p-0 mx-9">
-          <a class="d-flex align-items-center gap-2 list-group-item-action text-dark px-3 py-8 mb-1 rounded-1" href="javascript:void(0)">
-            <i class="ti ti-bookmark fs-5 text-success"></i>Sales Team </a>
-        </li>
-      </ul>
     </div>
   </div>
 </div>
